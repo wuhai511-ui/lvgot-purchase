5782b655a0e976cbb9360fc40597f2e2ef6dceac
Wuhai511 <wuhai511@gmail.com>
2026-03-27 10:58:34 +0800

    fix(h5): add vant css and uniapp tag css polyfills to fix layout issues

--- uniapp/src/main.js
+++ uniapp/src/main.js
@@ -2,6 +2,7 @@ import { createApp as createVueApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import vant from './plugins/vant'
+import 'vant/lib/index.css'   // Added vant CSS to fix styling
 import { router } from './router/index.js'
 import '@/utils/uni.js'
 
