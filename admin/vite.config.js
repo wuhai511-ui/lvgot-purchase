3cd7ade99d91a3891fdd1d7b0a8fd47f4ad9cb89
Wuhai511 <wuhai511@gmail.com>
2026-03-30 12:02:47 +0800

    fix(vite): 添加 @ alias 解决导入错误

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   base: './',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'static'
