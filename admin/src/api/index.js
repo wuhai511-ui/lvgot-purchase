6dfe0aeea8a85beee792ec8ef15f97655c1ce048
whf <whf@example.com>
2026-04-27 14:47:12 +0800

    fix: 更新 admin API 索引文件，移除不存在的 tour 和 finance 模块
    移除对不存在的 ./tour 和 ./finance 模块的导入，改用完整的现有模块列表：
    account, bank, bankCard, guide, merchant, order, recharge, split, store, system, tenant, trade, withdraw

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -1,18 +1,32 @@
 // admin/src/api/index.js
-import * as merchant from './merchant'
+import * as account from './account'
+import * as bank from './bank'
+import * as bankCard from './bankCard'
 import * as guide from './guide'
-import * as store from './store'
-import * as tour from './tour'
-import * as finance from './finance'
+import * as merchant from './merchant'
+import * as order from './order'
+import * as recharge from './recharge'
 import * as split from './split'
+import * as store from './store'
+import * as system from './system'
+import * as tenant from './tenant'
+import * as trade from './trade'
+import * as withdraw from './withdraw'
 
 export default {
-  merchant,
+  account,
+  bank,
+  bankCard,
   guide,
+  merchant,
+  order,
+  recharge,
+  split,
   store,
-  tour,
-  finance,
-  split
+  system,
+  tenant,
+  trade,
+  withdraw
 }
 
-export { merchant, guide, store, tour, finance, split }
+export { account, bank, bankCard, guide, merchant, order, recharge, split, store, system, tenant, trade, withdraw }
